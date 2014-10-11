8cfbead2891ef9c6a14f833f9e351acda2f1c2dc
omicronns <omicronns@gmail.com>
2014-10-11 20:41:54 +0200

    commit

--- gen/com/omicronns/mandelbrot/R.java
+++ gen/com/omicronns/mandelbrot/R.java
@@ -26,13 +26,8 @@ public final class R {
     }
     public static final class id {
         public static final int RootLayout=0x7f080000;
-        public static final int action_settings=0x7f080007;
-        public static final int buttonRender=0x7f080005;
-        public static final int editBottomY=0x7f080003;
-        public static final int editLeftX=0x7f080001;
-        public static final int editRightX=0x7f080002;
-        public static final int editTopY=0x7f080004;
-        public static final int mandelbrotView=0x7f080006;
+        public static final int action_settings=0x7f080002;
+        public static final int mandelbrotView=0x7f080001;
     }
     public static final class layout {
         public static final int activity_gen=0x7f030000;
@@ -43,11 +38,6 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int bottom_y_hint=0x7f050004;
-        public static final int left_x_hint=0x7f050002;
-        public static final int render_button_text=0x7f050006;
-        public static final int right_x_hint=0x7f050003;
-        public static final int top_y_hint=0x7f050005;
     }
     public static final class style {
         /** 
--- src/com/omicronns/mandelbrot/MandelbrotView.java
+++ src/com/omicronns/mandelbrot/MandelbrotView.java
@@ -1,19 +1,54 @@
 package com.omicronns.mandelbrot;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class MandelbrotView extends View {
-
-	private Paint setPainter;
-	byte[][] conv;
+	
+	private Bitmap btp;
+	private boolean generated = false;
+	
+	private int convergence(double re, double im) {
+		double sre = re;
+		double sim = im;
+		double sqre = re*re;
+		double sqim = im*im;
+		int iters = 0;
+		for(int k = 0; k < 128; k += 5)
+		{
+			if(sqre + sqim > 4)
+			{
+				iters = k;
+				break;
+			}
+			im = re*im;
+			im += im;
+			im += sim;
+			re = sqre - sqim + sre;
+			sqre = re*re;
+			sqim = im*im;
+		}
+		return (iters) | (iters << 8) | (iters << 16) | 0xff000000;
+	}
+	
+	private void generate(	int resX, int resY, double leftX, double rightX,
+							double bottomY, double topY) {
+		double width = rightX - leftX;
+		double height = topY - bottomY;
+		for(int i = 0; i < resX; ++i)
+		{
+			for(int j = 0; j < resY; ++j)
+			{
+				btp.setPixel(i, j, convergence(leftX + (width*i)/resX, bottomY + (height*j)/resY));
+			}
+		}
+	}
 	
 	private void init() {
-		setPainter = new Paint();
-		conv = MandelbrotThread.getConvergenceArray();
 	}
 	
 	public MandelbrotView(Context context) {
@@ -25,24 +60,16 @@ public class MandelbrotView extends View {
 		super(context, attrs);
 		init();
 	}
-
-	public MandelbrotView(Context context, AttributeSet attrs, int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-		init();
-	}
-
-	public void onDraw(Canvas canvas) {
-
-		setPainter.setARGB(255, 255, 0, 0);
-		setPainter.setStyle(Paint.Style.FILL_AND_STROKE);
-		setPainter.setStrokeWidth(1);
 	
-		for(int i = 0; i < 500; ++i)
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		if(!generated)
 		{
-			for(int j = 0; j < 500; ++j)
-			{
-				canvas.drawPoint(i, j, setPainter);
-			}
+			btp = Bitmap.createBitmap(getWidth(), getHeight(), Config.ARGB_8888);
+			generate(getWidth(), getHeight(), -2, 2, -(2*getHeight())/getWidth(), (2*getHeight())/getWidth());
+			generated = true;
 		}
+		canvas.drawBitmap(btp, getMatrix(), null);
 	}
 }
